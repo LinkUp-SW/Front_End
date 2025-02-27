bbaa32deb71260d8d356840c7cc664e7530fb26c
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-02-27 17:10:25 +0200

    added some details in the about me modal

--- src/pages/user_profile/components/AboutSection.tsx
+++ src/pages/user_profile/components/AboutSection.tsx
@@ -36,10 +36,22 @@ const AboutModal = () => {
   return (
     <div className="max-w-5xl md:w-[40rem] w-full ">
       <div className="flex flex-col mb-4">
-        <h2 className="text-xl font-semibold ">Modal Title</h2>
+        <h2 className="text-xl font-semibold ">Edit About</h2>
         <div className="w-full bg-gray-800 dark:bg-gray-300 h-[0.1rem] rounded-2xl " />
       </div>
-      <p>This is some modal content rendered generically.</p>
+      <section className="grid grid-cols-1 gap-1">
+        <p className="text-sm text-gray-500 dark:text-gray-300">
+          You can write about your years of experience, industry, or skills.
+          People also talk about their achievements or previous job experiences.
+        </p>
+        <textarea
+          maxLength={2600}
+          name="about"
+          id="about"
+          rows={10}
+          className="border-gray-50 border outline-1 outline-gray-400 dark:outline-gray-300 focus:outline-1 rounded-md text-sm p-2"
+        />
+      </section>
     </div>
   );
 };
