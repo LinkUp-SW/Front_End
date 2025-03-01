2464d6b3d3afe6510acfa3c61ef5bb301ab22ede
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-03-01 14:06:20 +0200

    new route:adding MessagingPage to the route.ts file to be rendered

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 // src/routes.ts
 
 import { RouteObject } from 'react-router-dom';
-import { LandingPage,FeedPage, UserProfilePage } from './pages';
+import { LandingPage,FeedPage, UserProfilePage,MessagingPage} from './pages';
+
 import React from 'react';
 
 // Define your routes as an array of RouteObject (compatible with React Router v6)
@@ -18,6 +19,10 @@ const routes: RouteObject[] = [
   {
     path: '/user-profile/:id',
     element: React.createElement(UserProfilePage)
+  },
+  {
+    path:'/messaging',
+    element: React.createElement(MessagingPage)
   }
 ];
 
