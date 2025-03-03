8b24325492232937f8c9571c44775561120c2cea
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-03 04:35:18 +0200

    code format: formatting the ExperienceSection.tsx code

--- src/pages/user_profile/components/ExperienceSection.tsx
+++ src/pages/user_profile/components/ExperienceSection.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const ExperienceSection = () => {
   return (
     <section className="bg-white dark:bg-gray-900 p-6 rounded-lg shadow">
-    <h2 className="text-xl text-black dark:text-white font-bold mb-4">Experience</h2>
-    <div className="space-y-4">
-      <div className="border-l-2 border-blue-600 pl-4">
-        <h3 className="font-bold">Senior Biomedical and Data Engineer</h3>
-        <p className="text-gray-600 dark:text-gray-300">Cairo University</p>
-        <p className="text-sm text-gray-500 dark:text-gray-200">Full-time</p>
+      <h2 className="text-xl text-black dark:text-white font-bold mb-4">
+        Experience
+      </h2>
+      <div className="space-y-4">
+        <div className="border-l-2 border-blue-600 pl-4">
+          <h3 className="font-bold">Senior Biomedical and Data Engineer</h3>
+          <p className="text-gray-600 dark:text-gray-300">Cairo University</p>
+          <p className="text-sm text-gray-500 dark:text-gray-200">Full-time</p>
+        </div>
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default ExperienceSection
+export default ExperienceSection;
