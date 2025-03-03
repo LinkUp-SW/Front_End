fa2dddfe68b7b1e382900efabbe7bef061fef013
AmrDoma <amrdoma123@outlook.com>
2025-03-03 16:21:05 +0200

    Component: Fixed StatsCard

--- src/components/feed/StatsCard.tsx
+++ src/components/feed/StatsCard.tsx
@@ -2,13 +2,13 @@ import { Card, CardContent } from "../ui/card";
 
 const StatsCard = () => {
   return (
-    <Card className="mb-4 p-4">
-      <CardContent className="text-gray-700">
-        <div className="flex justify-between text-sm">
+    <Card className="mb-2 bg-white border-0 w-full h-full">
+      <CardContent className="text-gray-900">
+        <div className="flex justify-between text-sm font-medium hover:underline hover:cursor-pointer">
           <span>Profile viewers</span>
           <span className="text-blue-600">27</span>
         </div>
-        <div className="flex justify-between text-sm mt-2">
+        <div className="flex justify-between text-sm mt-2 font-medium hover:underline hover:cursor-pointer">
           <span>Post impressions</span>
           <span className="text-blue-600">22</span>
         </div>
