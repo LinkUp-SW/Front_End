86db219cf2adf15b3058a7d463f621458e161111
AmrDoma <amrdoma123@outlook.com>
2025-03-03 13:54:54 +0200

    Created basic layout of FeedPage

--- src/pages/feed/FeedPage.tsx
+++ src/pages/feed/FeedPage.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import { UserList, WithNavBar } from '../../components'
+import React from "react";
+import { UserList, WithNavBar } from "../../components";
+import ProfileCard from "@/components/feed/ProfileCard";
+import StatsCard from "@/components/feed/StatsCard";
+import PremiumBanner from "@/components/feed/PremiumBanner";
+import Shortcuts from "@/components/feed/Shortcuts";
+import CreatePost from "@/components/feed/CreatePost";
+import Post from "@/components/feed/Post";
+import LinkedInNews from "@/components/feed/LinkedInNews";
 
 const FeedPage = () => {
   return (
-    <div>
-      <UserList/>
+    <div className="flex px-[10%]">
+      <div className="flex flex-col">
+        <ProfileCard />
+        <StatsCard />
+        <PremiumBanner />
+        <Shortcuts />
+      </div>
+      <div className="flex flex-col">
+        <CreatePost />
+        <Post name={"Doma"} content={"Hi"} />
+      </div>
+      <div className="flex flex-col">
+        <LinkedInNews />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WithNavBar(FeedPage)
+export default WithNavBar(FeedPage);
