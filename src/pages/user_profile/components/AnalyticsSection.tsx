a0aa8fce45e2e6bbae93f9127189b1b8778a2c45
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-03 04:35:01 +0200

    code format: formatting the AnalyticsSection.tsx code

--- src/pages/user_profile/components/AnalyticsSection.tsx
+++ src/pages/user_profile/components/AnalyticsSection.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const AnalyticsSection = () => {
   return (
     <section className="bg-white dark:bg-gray-900  p-6 rounded-lg shadow">
-    <h2 className="text-xl font-bold mb-4">Analytics</h2>
-    <div className="space-y-4">
-      <div>
-        <p className="font-medium">Profile views</p>
-        <p className="text-blue-600 font-bold">6</p>
+      <h2 className="text-xl font-bold mb-4">Analytics</h2>
+      <div className="space-y-4">
+        <div>
+          <p className="font-medium">Profile views</p>
+          <p className="text-blue-600 font-bold">6</p>
+        </div>
+        <div>
+          <p className="font-medium">Post impressions</p>
+          <p className="text-blue-600 font-bold">7</p>
+        </div>
       </div>
-      <div>
-        <p className="font-medium">Post impressions</p>
-        <p className="text-blue-600 font-bold">7</p>
-      </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default AnalyticsSection
+export default AnalyticsSection;
