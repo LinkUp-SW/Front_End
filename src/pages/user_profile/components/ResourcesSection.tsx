93d55cc679599131fc44737fdd80ddf062ce64d4
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-03 04:35:32 +0200

    code format: formatting the ResourcesSection.tsx code

--- src/pages/user_profile/components/ResourcesSection.tsx
+++ src/pages/user_profile/components/ResourcesSection.tsx
@@ -1,12 +1,12 @@
 const ResourcesSection = () => {
   return (
     <section className="bg-white dark:bg-gray-900 p-6 rounded-lg shadow">
-    <h2 className="text-xl font-bold mb-4">Resources</h2>
-    <p className="text-blue-600 dark:text-blue-300 text-sm">
-      Show all analytics →
-    </p>
-  </section>
-  )
-}
+      <h2 className="text-xl font-bold mb-4">Resources</h2>
+      <p className="text-blue-600 dark:text-blue-300 text-sm">
+        Show all analytics →
+      </p>
+    </section>
+  );
+};
 
-export default ResourcesSection
+export default ResourcesSection;
