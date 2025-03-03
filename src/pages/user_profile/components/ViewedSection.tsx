b6a416c1d043895ca52c63c52b02de1c12a4bf02
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-03 04:35:50 +0200

    code format: formatting the ViewedSection.tsx code

--- src/pages/user_profile/components/ViewedSection.tsx
+++ src/pages/user_profile/components/ViewedSection.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const ViewedSection = () => {
   return (
     <section className="bg-white dark:bg-gray-900  p-6 rounded-lg shadow">
-    <h2 className="text-xl font-bold mb-4 text-black dark:text-white">Viewers also viewed</h2>
-    <div className="space-y-4">
-      {/* List of viewed profiles */}
-      {[1, 2, 3].map((item) => (
-        <div key={item} className="flex items-center gap-3">
-          <div className="w-10 h-10 rounded-full bg-gray-200" />
-          <div>
-            <p className="font-medium text-gray-900 dark:text-gray-200">John Doe</p>
-            <p className="text-sm text-gray-600 dark:text-gray-300">Software Engineer</p>
+      <h2 className="text-xl font-bold mb-4 text-black dark:text-white">
+        Viewers also viewed
+      </h2>
+      <div className="space-y-4">
+        {/* List of viewed profiles */}
+        {[1, 2, 3].map((item) => (
+          <div key={item} className="flex items-center gap-3">
+            <div className="w-10 h-10 rounded-full bg-gray-200" />
+            <div>
+              <p className="font-medium text-gray-900 dark:text-gray-200">
+                John Doe
+              </p>
+              <p className="text-sm text-gray-600 dark:text-gray-300">
+                Software Engineer
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  </section>
-  )
-}
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default ViewedSection
+export default ViewedSection;
