f6575bde626365fadf83df1c2c2d22e8e71402a7
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-05 02:31:26 +0200

    feat(experience card): adding the pen and plus icons to then be able to open modals that either edits or add a new experience to the user profile

--- src/pages/user_profile/components/ExperienceSection.tsx
+++ src/pages/user_profile/components/ExperienceSection.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import { handleOpenModalType } from "../../../utils";
+import { useDispatch } from "react-redux";
+import { BsPencil } from "react-icons/bs";
+import { GoPlus } from "react-icons/go";
 
 const ExperienceSection = () => {
+  const dispatch = useDispatch();
+
+  const handleOpenModal = () => {
+    dispatch(handleOpenModalType("experience")); // Dispatch a string identifier or an object with modal details
+  };
+
   return (
     <section className="bg-white dark:bg-gray-900 p-6 rounded-lg shadow">
-      <h2 className="text-xl text-black dark:text-white font-bold mb-4">
-        Experience
-      </h2>
+      <header className="flex justify-between items-center mb-4">
+        <h2 className="text-xl text-black dark:text-white font-bold">Experience</h2>
+       <div className="flex items-center gap-2">
+       <button
+           onClick={handleOpenModal}
+           id="edit-button"
+           className="hover:bg-gray-300 dark:hover:text-black rounded-full cursor-pointer transition-all duration-200 ease-in-out"
+         >
+           <span className="hidden">add button</span>
+           <GoPlus size={30} />
+         </button>
+         <button
+           onClick={handleOpenModal}
+           id="edit-button"
+           className="hover:bg-gray-300 dark:hover:text-black p-2 rounded-full cursor-pointer transition-all duration-200 ease-in-out"
+         >
+           <span className="hidden">edit button</span>
+           <BsPencil size={20} />
+         </button>
+       </div>
+      </header>
       <div className="space-y-4">
         <div className="border-l-2 border-blue-600 pl-4">
           <h3 className="font-bold">Senior Biomedical and Data Engineer</h3>
