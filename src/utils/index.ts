d31dc49293cf4c7709ee36110e1762c83cea75e1
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-05 02:28:03 +0200

    new Function: added a new utility function that takes the modal type as a param and accordingly render a specific modal accordingly

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
-export const convertStringsArrayToLowerCase=(arr:string[])=>{
-    return arr.map((el) => el.toLocaleLowerCase())
-}
+import { openModal } from "../slices/modal/modalSlice";
+
+export const convertStringsArrayToLowerCase = (arr: string[]) => {
+  return arr.map((el) => el.toLocaleLowerCase());
+};
+
+export const handleOpenModalType = (modalType: string) => {
+  return openModal(modalType);
+};
