65a71ba182ffddfbe0b165f6497dc035cce79bae
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-05 02:28:54 +0200

    new constants: added a new constan array that correspond to the user profile section the user can choose and add from them

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ import { GoHomeFill } from "react-icons/go";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { FaBell, FaBriefcase, FaUserAlt } from "react-icons/fa";
 import { BsChatDotsFill } from "react-icons/bs";
+import { handleOpenModalType } from "../utils";
 
 export const LARGE_SCREEN_NAV_ITEMS = [
   {
@@ -56,5 +57,43 @@ export const SMALL_SCREEN_NAV_ITEMS = [
     path: "/jobs",
     icon: FaBriefcase,
     title: "Jobs",
+  },
+];
+
+export const CORE_PROFILE_SECTIONS = [
+  {
+    id: 1,
+    title: "Add profile photo",
+    onClickEvent: handleOpenModalType("profile_photo"),
+  },
+  {
+    id: 2,
+    title: "Add about",
+    onClickEvent: handleOpenModalType("about"),
+  },
+  {
+    id: 3,
+    title: "Add education",
+    onClickEvent: handleOpenModalType("education"),
+  },
+  {
+    id: 4,
+    title: "Add experience",
+    onClickEvent: handleOpenModalType("experience"),
+  },
+  {
+    id: 5,
+    title: "Add services",
+    onClickEvent: handleOpenModalType("services"),
+  },
+  {
+    id: 6,
+    title: "Add career break",
+    onClickEvent: handleOpenModalType("career_break"),
+  },
+  {
+    id: 7,
+    title: "Add skills",
+    onClickEvent: handleOpenModalType("skills"),
   },
 ];
