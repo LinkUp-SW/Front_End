1de9c636da1d173d937bb89c61a54ac980aad59e
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-12 02:51:39 +0200

    new global types: added the experience types took from the backend to be later used in the mock server to memic the real api request and response

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,30 @@
-export interface test{
-    name:string
+export interface Experience {
+  _id: string;
+  title: string;
+  employee_type: string;
+  company: string;
+  is_current: boolean;
+  start_date: Date;
+  end_date?: Date;
+  location: string;
+  description: string;
+  location_type: string;
+  where_did_you_find_us: string;
+  skills: string[];
+  media: Media;
+}
+
+export interface Skill {
+  name: string;
+  endorsments: string[];
+  used_where: [
+    education: string[],
+    certificate: string[],
+    experience: string[]
+  ];
+}
+
+export interface Media {
+  image: string[];
+  video: string[];
 }
