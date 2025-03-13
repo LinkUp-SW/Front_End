2d314b5972872fe0e0ba6e091df5e48cda8405ae
NadaOmar03 <nada.mohammad03@eng-st.cu.edu.eg>
2025-03-14 01:13:00 +0200

    Enhance the UI of RemoveConnectionModal: center text inside RemoveConnectionModal and  adjust the modal size for larger screens while maintaining responsiveness

--- src/pages/mynetwork/components/modals/remove_connection_modal/RemoveConnectionModal.tsx
+++ src/pages/mynetwork/components/modals/remove_connection_modal/RemoveConnectionModal.tsx
@@ -15,25 +15,25 @@ const RemoveConnectionModal: React.FC<RemoveConnectionModalProps> = ({
   const dispatch = useDispatch();
 
   return (
-    <div className="w-full flex items-center justify-center md:w-40">
-      <div className="p-6 bg-white rounded-lg shadow-lg">
+    <div className="w-full flex items-center justify-center">
+      <div className="p-6 w-full md:w-96 lg:w-[30rem] text-center">
         <h2 className="text-xl font-semibold">Remove Connection</h2>
         <p className="mt-2">
           Are you sure you want to remove <strong>{userData.userName}</strong>?
         </p>
-        <div className="mt-4 flex gap-4">
+        <div className="mt-4 flex justify-center gap-4">
           <button
             onClick={() => {
               onConfirm();
               dispatch(closeModal());
             }}
-            className="px-4 py-2 bg-red-600 text-white rounded"
+            className="px-4 py-2 bg-red-600 text-white rounded cursor-pointer"
           >
             Confirm
           </button>
           <button
             onClick={() => dispatch(closeModal())}
-            className="px-4 py-2 bg-gray-300 rounded"
+            className="px-4 py-2 bg-gray-300 rounded cursor-pointer"
           >
             Cancel
           </button>
