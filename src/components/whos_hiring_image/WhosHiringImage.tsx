f2509b9f6d9c83c78fe5dfae4b169bbe8ae829c4
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-14 16:49:42 +0200

    fixed with of the imag ethat takes the full width of its container

--- src/components/whos_hiring_image/WhosHiringImage.tsx
+++ src/components/whos_hiring_image/WhosHiringImage.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const WhosHiringImage = () => {
   return (
-    <div className="mt-4 cursor-pointer">
+    <div className="mt-4 cursor-pointer h-fit">
       <img
         src="./src/assets/see_who's_hiring.jpg"
         alt="Promotional Banner"
-        className="rounded-lg shadow-lg w-full object-cover"
+        className="rounded-lg shadow-lg w-58 aspect-square object-fill object-center"
       />
     </div>
   );
