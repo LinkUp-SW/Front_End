ba52db10a0dd7e19a3a15c8a1301d1e51d59b75c
AmrDoma <amrdoma123@outlook.com>
2025-03-14 02:01:21 +0200

    Refactor: refactored code to be less repetitive, following best practice

--- src/pages/feed/components/LinkedInFooter.tsx
+++ src/pages/feed/components/LinkedInFooter.tsx
@@ -1,40 +1,24 @@
 import React from "react";
+import { FOOTER_LINKS } from "@/constants";
+import { FaSortDown } from "react-icons/fa";
 
 const LinkedInFooter: React.FC = () => {
   return (
-    <footer className=" text-gray-600 text-xs p-4 right-5 w-64 sticky top-18 dark:text-neutral-400">
+    <footer className=" text-gray-600 text-xs p-4 right-14 w-64 sticky top-18 dark:text-neutral-400">
       <div className="flex flex-wrap justify-center gap-3">
-        <a href="#" className="hover:underline">
-          About
-        </a>
-        <a href="#" className="hover:underline">
-          Accessibility
-        </a>
-        <a href="#" className="hover:underline">
-          Help Center
-        </a>
-        <a href="#" className="hover:underline">
-          Privacy & Terms
-        </a>
-        <a href="#" className="hover:underline">
-          Ad Choices
-        </a>
-        <a href="#" className="hover:underline">
-          Advertising
-        </a>
-        <a href="#" className="hover:underline">
-          Business Services
-        </a>
-        <a href="#" className="hover:underline">
-          Get the LinkUp app
-        </a>
-        <a href="#" className="hover:underline">
-          More
-        </a>
+        {FOOTER_LINKS.map((link, index) => (
+          <span
+            key={index}
+            className="cursor-pointer relative right-2 hover:text-blue-600 dark:hover:text-blue-400 hover:underline flex items-center"
+          >
+            <span>{link.text}</span>
+            <span>{link.hasArrow && <FaSortDown size={12} />}</span>
+          </span>
+        ))}
       </div>
 
       {/* Copyright Section */}
-      <div className="mt-2 flex justify-center items-center">
+      <div className="mt-2 flex justify-center relative right-3 pt-2 items-center">
         <img className="w-5 h-5" src="../../../public/link_up_logo.png"></img>
         <span className="ml-2">LinkUp Corporation © 2025</span>
       </div>
