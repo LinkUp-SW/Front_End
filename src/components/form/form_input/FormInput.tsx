fbe65aa99e8648c21af390ae7a8a4f8d6f85c488
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-15 07:15:53 +0200

    adding the form input error state and message

--- src/components/form/form_input/FormInput.tsx
+++ src/components/form/form_input/FormInput.tsx
@@ -4,6 +4,8 @@ interface FormInputProps {
   value: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   helperText?: string;
+  helperTextType?: string;
+  type?: string;
 }
 const FormInput: React.FC<FormInputProps> = ({
   label,
@@ -11,18 +13,28 @@ const FormInput: React.FC<FormInputProps> = ({
   value,
   onChange,
   helperText,
+  helperTextType,
+  type,
 }) => (
   <div className="flex flex-col gap-2 pt-5">
     <p className="text-sm text-gray-500 dark:text-gray-400">{label}</p>
     <input
-      type="text"
+      type={type || "text"}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
       className="outline-gray-600 border p-2 rounded-md text-sm transition-all duration-300 ease-in-out dark:hover:border-white hover:border-black border-gray-600 focus:outline-0"
     />
     {helperText && (
-      <p className="text-xs text-gray-500 dark:text-gray-400">{helperText}</p>
+      <p
+        className={`text-xs   ${
+          helperTextType?.toLowerCase() === "error"
+            ? "text-red-500 dark:text-red-400"
+            : "text-gray-500 dark:text-gray-400"
+        }`}
+      >
+        {helperText}
+      </p>
     )}
   </div>
 );
