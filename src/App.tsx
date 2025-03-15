3815d37b0914e1a52f6c5a24f9561ce0af4e7be7
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-15 03:26:25 +0200

    feat: added the theme change color to the Toast Component and added rich color attribute to the Toast component for it to accept the styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import { ScreenWidthListener, ThemeListener, Toaster } from "./components";
+import { useSelector } from "react-redux";
+import { RootState } from "./store";
 
 const App: React.FC = () => {
+  const theme = useSelector((state: RootState) => state.theme.theme);
   return (
     <>
       <ScreenWidthListener />
@@ -20,7 +23,7 @@ const App: React.FC = () => {
           })}
         </Routes>
       </Router>
-      <Toaster />
+      <Toaster richColors theme={theme} />
     </>
   );
 };
