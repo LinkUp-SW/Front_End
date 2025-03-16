e40cf0059583108ecdcd1d220033c029084e2041
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-16 04:50:26 +0200

    feat: new component, the second step of the multistep process where the user will enter his first name and last name

---
+++ src/pages/auth/sign_up/multi_step/FirstNameLastNameForm.tsx
@@ -0,0 +1,59 @@
+import { FormInput } from '@/components';
+import React from 'react'
+
+type FirstNameLastNameData = {
+  firstName: string;
+  lastName: string;
+};
+
+type FirstNameLastNameProps = FirstNameLastNameData & {
+  updateFields: (fields: Partial<FirstNameLastNameData>) => void;
+  saveCredentials:()=>void;
+};
+
+
+
+const FirstNameLastNameForm = ({
+  firstName,
+  lastName,
+  updateFields,
+  saveCredentials
+}:FirstNameLastNameProps) => {
+  return (
+    <div className="sm:w-full">
+    <div className="space-y-1">
+      <FormInput
+        label="firstName"
+        placeholder="Enter your First Name"
+        value={firstName}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          updateFields({ firstName: e.target.value });
+        }}
+        type="text"
+      />
+      <div className="w-full relative">
+        <FormInput
+          label="lastName"
+          placeholder="Enter your Last Name"
+          value={lastName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            updateFields({ lastName: e.target.value });
+          }}
+          type="password"
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={saveCredentials}
+          className="flex disabled:opacity-75 disabled:bg-indigo-500 disabled:cursor-not-allowed cursor-pointer w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-700 dark:hover:bg-indigo-600 transition-all duration-300 ease-in-out"
+        >
+          Continue
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default FirstNameLastNameForm
