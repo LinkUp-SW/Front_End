80aee29e6b82f823aed812c2cff99965889ba725
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-16 04:47:26 +0200

    new util function: extracted the validateEmail regex to a utility functtion to be used accross multiple files and added a new util function for the password regex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,30 @@ export const convertStringsArrayToLowerCase = (arr: string[]): string[] => {
   return arr.map((el) => el.toLocaleLowerCase());
 };
 
+export const validateEmail=(email:string):boolean=>{
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email)
+}
+
+export const validatePassword = (password: string): string | null => {
+  if (password.length < 8) {
+    return "Password must be at least 8 characters long";
+  }
+  if (!/[A-Z]/.test(password)) {
+    return "Password must contain at least one uppercase letter";
+  }
+  if (!/[a-z]/.test(password)) {
+    return "Password must contain at least one lowercase letter";
+  }
+  if (!/\d/.test(password)) {
+    return "Password must contain at least one digit";
+  }
+  if (!/[^A-Za-z0-9]/.test(password)) {
+    return "Password must contain at least one special character";
+  }
+  return null;
+};
+
 // Annotate the function so TypeScript can infer the generic type properly.
 export const handleOpenModalType = <T>(
   modalType: string,
