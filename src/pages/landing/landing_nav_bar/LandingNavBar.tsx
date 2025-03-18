583ebcf43fd9bc7437125d351c7d38fc04538606
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-19 00:16:20 +0200

    hot fixes: added the ids for the links and buttons for easy accessability by the testing team

--- src/pages/landing/landing_nav_bar/LandingNavBar.tsx
+++ src/pages/landing/landing_nav_bar/LandingNavBar.tsx
@@ -25,11 +25,16 @@ const LandingNavBar = () => {
           width >= 768 ? "flex" : "hidden"
         } space-x-4 items-center"`}
       >
-        <Link to={'/signup'} className="text-gray-700 font-semibold cursor-pointer px-5 py-1.5 hover:bg-gray-200 rounded-full transition-all duration-300 ease-in-out dark:text-gray-300 dark:hover:bg-gray-700">
+        <Link
+          id="login-now-link"
+          to={"/signup"}
+          className="text-gray-700 font-semibold cursor-pointer grid place-items-center px-5 py-1.5 hover:bg-gray-200 rounded-full transition-all duration-300 ease-in-out dark:text-gray-300 dark:hover:bg-gray-700"
+        >
           Join now
         </Link>
         <Link
           to={"/login"}
+          id="join-now-link"
           className="border border-blue-600 font-semibold cursor-pointer text-blue-600 px-4 py-2 rounded-full hover:text-blue-700 hover:bg-blue-100 transition-all duration-300 ease-in-out dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-500/20"
         >
           Sign in
@@ -55,11 +60,16 @@ const LandingNavBar = () => {
       >
         <div className="m-4 p-4 flex flex-col space-y-3 items-center">
           <ThemeToggle />
-          <Link to={'/signup'} className="w-full text-gray-700 font-semibold cursor-pointer px-5 py-1.5 hover:bg-gray-200 rounded-full transition-all duration-300 ease-in-out dark:text-gray-300 dark:hover:bg-gray-700">
+          <Link
+            id="join-now-link"
+            to={"/signup"}
+            className="w-full text-gray-700 font-semibold cursor-pointer px-5 py-1.5 hover:bg-gray-200 rounded-full transition-all duration-300 ease-in-out dark:text-gray-300 dark:hover:bg-gray-700"
+          >
             Join now
           </Link>
           <Link
             to={"/login"}
+            id="login-now-link"
             className="w-full border text-center border-blue-600 font-semibold cursor-pointer text-blue-600 px-4 py-2 rounded-full hover:text-blue-700 hover:bg-blue-100 transition-all duration-300 ease-in-out dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-500/20"
           >
             Sign in
