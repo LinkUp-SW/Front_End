01647cb9e5a5f83be1ac932a9fda205733b90dc3
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-19 00:20:07 +0200

    new functions: added new functions for http requests regarding the user auth

--- src/endpoints/userAuth.ts
+++ src/endpoints/userAuth.ts
@@ -1,15 +1,49 @@
+import { UserStarterInterface } from "@/types";
 import axios from "axios";
 
-export const signin = async (
-  identifier: string,
-  password: string,
-  captchaToken: string
-) => {
-  const response = await axios.post("/api/v1/signin", {
-    identifier,
+export const signin = async (identifier: string, password: string) => {
+  const response = await axios.post("http://localhost:3000/auth/login", {
+    email: identifier,
     password,
-    captchaToken,
   });
+  return response.data;
+};
+
+export const verifyEmail = async (email: string) => {
+  const response = await axios.post(
+    "http://localhost:3000/api/v1/user/verify-email",
+    { email }
+  );
 
   return response.data;
+};
+
+export const createUserAccount = async (
+  userCredentials: UserStarterInterface
+) => {
+  const response = await axios.post(
+    "http://localhost:3000/api/v1/user/signup/starter",
+    { ...userCredentials }
+  );
+  return response.data;
+};
+
+export const sendOTP = async (email: string) => {
+  const response = await axios.post(
+    "http://localhost:3000/api/v1/user/send-otp",
+    { email: email }
+  );
+  return response.data;
+};
+
+export const verifyOTP = async (otp: string) => {
+  const response = await axios.post(
+    "http://localhost:3000/api/v1/user/verify-otp",
+    { otp: otp }
+  );
+  return response.data;
+};
+
+export const initiateGoogleAuth = () => {
+  window.location.href = "http://localhost:3000/auth/google";
 };
