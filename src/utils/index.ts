d85950d8b38c37a35ad0efaa8e16354170f02c43
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-19 03:38:56 +0200

    new utils: added a new utility function that handles the saving proccess of the user starter credentials across the signup process

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,17 @@
+//utils/index.ts
+
 import { openModal } from "@/slices/modal/modalSlice";
+import { UserStarterInterface } from "@/types";
 
 // Example utility function to convert an array of strings to lowercase
 export const convertStringsArrayToLowerCase = (arr: string[]): string[] => {
   return arr.map((el) => el.toLocaleLowerCase());
 };
 
-export const validateEmail=(email:string):boolean=>{
+export const validateEmail = (email: string): boolean => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email)
-}
+  return emailRegex.test(email);
+};
 
 export const validatePassword = (password: string): string | null => {
   if (password.length < 8) {
@@ -32,7 +35,34 @@ export const validatePassword = (password: string): string | null => {
 // Annotate the function so TypeScript can infer the generic type properly.
 export const handleOpenModalType = <T>(
   modalType: string,
-  modalData: T|null=null
+  modalData: T | null = null
 ): ReturnType<typeof openModal> => {
   return openModal({ modalType, modalData });
 };
+
+export const handleSaveCredentials = (userCredentials:Partial<UserStarterInterface>) => {
+    const savedCredential = localStorage.getItem("user-signup-credentials");
+
+    if (!savedCredential) {
+      const toBeStoredUserCredentials = JSON.stringify(userCredentials);
+      return localStorage.setItem(
+        "user-signup-credentials",
+        toBeStoredUserCredentials
+      );
+    }
+
+    // Convert the string from localStorage into an object
+    const parsedCredentials = JSON.parse(savedCredential);
+    // Merge the parsed credentials with the new userCredentials
+    const newToBeStoredCredentials = {
+      ...parsedCredentials,
+      ...userCredentials,
+    };
+
+    // Save the updated credentials back to localStorage as a string
+    localStorage.setItem(
+      "user-signup-credentials",
+      JSON.stringify(newToBeStoredCredentials)
+    );
+  };
+
