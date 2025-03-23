271351726a34fe388b22d36301bf02e5d55fbc0c
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-23 03:19:57 +0200

    refactor: refactored the msw handlers to separate the endpoints into different files for readability and scalability and avoid future conflicts

---
+++ src/mocks/handler_wrapper/getHandler.ts
@@ -0,0 +1,14 @@
+import { http, HttpResponse, JsonBodyType } from "msw";
+
+export function createGetHandler<T extends JsonBodyType>(
+  path: string,
+  resolver: (req: { params: Record<string, unknown> }) => T | HttpResponse
+) {
+  return http.get(path, async (req) => {
+    console.log(`[MSW] Intercepted GET ${req}`);
+    const result = resolver({ params: req.params });
+    return result instanceof HttpResponse
+      ? result
+      : HttpResponse.json<T>(result);
+  });
+}
