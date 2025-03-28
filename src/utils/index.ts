887bbd3da91fa6a9a8ff2bf86ee3409a531b6b27
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-28 22:19:10 +0200

    hot fix: added more strict regex email validator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -26,9 +26,9 @@ export const validateEmail = (email: string): boolean => {
   // @                     --> The @ symbol.
   // (?:(?!-)[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,})
   //                        --> Domain part: labels can’t start with a hyphen, and the TLD must be at least 2 letters.
-  const emailRegex =
-    /^(?=.{1,254}$)(?=[^@]{1,64}@)[a-zA-Z0-9][a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]*(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:(?=[a-z0-9-]{1,63}\.)[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+(?=[a-z0-9-]{2,63}$)[a-z0-9](?!.*--)(?:[a-z0-9-]{0,61}[a-z0-9])?$/i;
-
+  const emailRegex = 
+  /^(?=.{1,254}$)(?=[^@]{1,64}@)[a-zA-Z0-9][a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]*(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:(?=[a-z0-9]{1,63}\.)[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9])?\.)+(?=[a-z0-9]{2,63}$)[a-z0-9]{2,63}$/i;
+  
   return emailRegex.test(email);
 };
 
