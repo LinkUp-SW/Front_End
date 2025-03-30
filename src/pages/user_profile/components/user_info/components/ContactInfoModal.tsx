f63f0a512f363bb25cf08a488b45255a5f690e09
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-30 14:04:45 +0200

    feat: enhance ContactInfoModal to display additional contact details and handle empty states

--- src/pages/user_profile/components/user_info/components/ContactInfoModal.tsx
+++ src/pages/user_profile/components/user_info/components/ContactInfoModal.tsx
@@ -8,16 +8,15 @@ import {
 } from "@/components";
 import { Link } from "react-router-dom";
 import { formatIsoDateToHumanReadable } from "@/utils";
-import { FaBirthdayCake } from "react-icons/fa";
+import { FaBirthdayCake, FaPhone, FaGlobe, FaMobileAlt } from "react-icons/fa";
 import { CiHome } from "react-icons/ci";
+import { Link as LinkIcon } from "lucide-react";
 
 import type { ContactInfo } from "@/types";
-import { Link as LinkIcon } from "lucide-react";
 
 type ContactInfoModalProps = {
   user: {
     contact_info: ContactInfo;
-    website: string;
     first_name: string;
     last_name: string;
   };
@@ -27,41 +26,79 @@ type ContactInfoModalProps = {
 export const ContactInfoModal = ({
   user,
   triggerLabel,
-}: ContactInfoModalProps) => (
-  <Dialog>
-    <DialogTrigger asChild>
-      <button className="text-blue-600 cursor-pointer text-sm font-semibold dark:text-blue-400">
-        {triggerLabel}
-      </button>
-    </DialogTrigger>
-    <DialogContent className="dark:bg-gray-900">
-      <DialogHeader>
-        <DialogTitle className="text-2xl underline">
-          {user.first_name} {user.last_name}
-        </DialogTitle>
-      </DialogHeader>
-      <div className="grid gap-6">
-        <h2 className="font-bold text-xl">Contact Info</h2>
-        <ContactInfoItem
-          icon={<LinkIcon />}
-          label="Website"
-          value={<Link to={user.website}>{user.website}</Link>}
-        />
-        {user.contact_info.birthday&&<ContactInfoItem
-          icon={<FaBirthdayCake />}
-          label="Birthday"
-          value={formatIsoDateToHumanReadable(user.contact_info.birthday)}
-        />}
-        
-        <ContactInfoItem
-          icon={<CiHome />}
-          label="Address"
-          value={user.contact_info.address}
-        />
-      </div>
-    </DialogContent>
-  </Dialog>
-);
+}: ContactInfoModalProps) => {
+  const { website, birthday, address, phone_number, country_code, phone_type } =
+    user.contact_info;
+
+  // Check if there is any contact info available
+  const hasAnyInfo =
+    website ||
+    birthday ||
+    address ||
+    phone_number ||
+    country_code ||
+    phone_type;
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="text-blue-600 cursor-pointer text-sm font-semibold dark:text-blue-400">
+          {triggerLabel}
+        </button>
+      </DialogTrigger>
+      <DialogContent className="dark:bg-gray-900">
+        <DialogHeader>
+          <DialogTitle className="text-2xl underline">
+            {user.first_name} {user.last_name}
+          </DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-6">
+          <h2 className="font-bold text-xl">Contact Info</h2>
+          {hasAnyInfo ? (
+            <>
+              <ContactInfoItem
+                icon={<FaPhone />}
+                label="Phone Number"
+                value={phone_number !== null ? phone_number : "N/A"}
+              />
+              <ContactInfoItem
+                icon={<FaGlobe />}
+                label="Country Code"
+                value={country_code || "N/A"}
+              />
+              <ContactInfoItem
+                icon={<FaMobileAlt />}
+                label="Phone Type"
+                value={phone_type || "N/A"}
+              />
+              <ContactInfoItem
+                icon={<LinkIcon />}
+                label="Website"
+                value={website ? <Link to={website}>{website}</Link> : "N/A"}
+              />
+              <ContactInfoItem
+                icon={<FaBirthdayCake />}
+                label="Birthday"
+                value={
+                  birthday ? formatIsoDateToHumanReadable(birthday) : "N/A"
+                }
+              />
+              <ContactInfoItem
+                icon={<CiHome />}
+                label="Address"
+                value={address || "N/A"}
+              />
+            </>
+          ) : (
+            <p className="text-sm font-semibold text-gray-700 dark:text-gray-400">
+              No contact info available
+            </p>
+          )}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 const ContactInfoItem = ({
   icon,
