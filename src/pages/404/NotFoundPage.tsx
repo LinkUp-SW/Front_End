e3be7321749fe94ffc647528d15023336108a312
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-31 19:00:34 +0200

    feat: wrap NotFoundPage in EmailVerificationLayout for improved user experience

--- src/pages/404/NotFoundPage.tsx
+++ src/pages/404/NotFoundPage.tsx
@@ -1,31 +1,33 @@
-import { UserAuthLayout } from "@/components";
 import { useNavigate } from "react-router-dom";
+import EmailVerificationLayout from "../auth/components/EmailVerificationLayout";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="flex min-h-full w-full max-w-md flex-col gap-4 justify-center items-center p-4">
-      <header className="text-center">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
-          404
-        </h1>
-        <p className="text-xl font-semibold text-gray-600 dark:text-gray-300">
-          Page Not Found
+    <EmailVerificationLayout>
+      <main className="flex min-h-full w-full max-w-md flex-col gap-4 justify-center items-center p-4">
+        <header className="text-center">
+          <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
+            404
+          </h1>
+          <p className="text-xl font-semibold text-gray-600 dark:text-gray-300">
+            Page Not Found
+          </p>
+        </header>
+        <p className="text-center text-sm text-gray-500 dark:text-gray-400">
+          Sorry, the page you are looking for does not exist.
         </p>
-      </header>
-      <p className="text-center text-sm text-gray-500 dark:text-gray-400">
-        Sorry, the page you are looking for does not exist.
-      </p>
-      <button
-        type="button"
-        onClick={() => navigate("/")}
-        className="mt-4 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline-2 cursor-pointer focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-700 dark:hover:bg-indigo-600 transition-all duration-300 ease-in-out"
-      >
-        Go Home
-      </button>
-    </main>
+        <button
+          type="button"
+          onClick={() => navigate("/")}
+          className="mt-4 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline-2 cursor-pointer focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-700 dark:hover:bg-indigo-600 transition-all duration-300 ease-in-out"
+        >
+          Go Home
+        </button>
+      </main>
+    </EmailVerificationLayout>
   );
 };
 
-export default UserAuthLayout(NotFoundPage);
+export default NotFoundPage;
