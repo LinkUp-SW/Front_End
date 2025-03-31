519797a6c4c1e5969403df78bc23b10032f67940
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-03-31 19:15:44 +0200

    refactor: simplify ReplyProps by equating it directly with CommentProps

--- src/pages/feed/components/Reply.tsx
+++ src/pages/feed/components/Reply.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import Comment, { CommentProps } from "./Comment";
 
 // Extend CommentProps so Reply can use all the same properties
-interface ReplyProps extends CommentProps {
-  // Optionally add extra props specific for replies
-}
+// interface ReplyProps extends CommentProps {
+//   // Optionally add extra props specific for replies
+// }
+
+// there was an error in the extend as extends require new attributes
+// so when no new properties or attributes TS suggest to just equate the new type with the old type
+type ReplyProps = CommentProps;
 
 const Reply: React.FC<ReplyProps> = (props) => {
   return (
