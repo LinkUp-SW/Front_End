ed9d5210bb80c1332b40e58041a25bbd5e085633
AmrDoma <amrdoma123@outlook.com>
2025-04-02 21:00:08 +0200

    fix: returned correct auth code after temporarily disabling it

--- src/components/hoc/AuthMiddleware.tsx
+++ src/components/hoc/AuthMiddleware.tsx
@@ -15,60 +15,60 @@ const AuthMiddleware = ({ children }: AuthMiddlewareProps) => {
   const token = Cookies.get("linkup_auth_token");
   const myUserId = Cookies.get("linkup_user_id");
 
-  // useEffect(() => {
-  //   let isMounted = true;
-  //   const controller = new AbortController();
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
 
-  const checkAuth = async () => {
-    try {
-      if (!token || !myUserId) {
-        await userLogOut();
-        navigate("/login", { replace: true });
-        return;
-      }
-
-      //       const response = await validateAuthToken(token, {
-      //         signal: controller.signal,
-      //       });
-
-      if (isMounted) {
-        if (!response.success || !myUserId) {
-          throw new Error("Invalid token");
-        }
-        setIsVerified(true);
-      }
-    } catch (error: unknown) {
-      if (isMounted) {
-        Cookies.remove("linkup_auth_token");
-        Cookies.remove("linkup_user_id");
-
-        let redirectPath = "/login";
-        if (error instanceof AxiosError) {
-          console.error("API Error:", error.response?.data);
-          redirectPath += "?error=session_expired";
-        } else if (error instanceof Error) {
-          console.error("Auth Error:", error.message);
+    const checkAuth = async () => {
+      try {
+        if (!token || !myUserId) {
+          await userLogOut();
+          navigate("/login", { replace: true });
+          return;
         }
 
-        navigate(redirectPath, {
-          replace: true,
-          state: { from: location.pathname }, // Preserve current location
+        const response = await validateAuthToken(token, {
+          signal: controller.signal,
         });
+
+        if (isMounted) {
+          if (!response.success || !myUserId) {
+            throw new Error("Invalid token");
+          }
+          setIsVerified(true);
+        }
+      } catch (error: unknown) {
+        if (isMounted) {
+          Cookies.remove("linkup_auth_token");
+          Cookies.remove("linkup_user_id");
+
+          let redirectPath = "/login";
+          if (error instanceof AxiosError) {
+            console.error("API Error:", error.response?.data);
+            redirectPath += "?error=session_expired";
+          } else if (error instanceof Error) {
+            console.error("Auth Error:", error.message);
+          }
+
+          navigate(redirectPath, {
+            replace: true,
+            state: { from: location.pathname }, // Preserve current location
+          });
+        }
       }
-    }
-  };
+    };
 
-  //   checkAuth();
+    checkAuth();
 
-  //   return () => {
-  //     isMounted = false;
-  //     controller.abort();
-  //   };
-  // }, [navigate, token]);
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [navigate, token]);
 
-  // if (!isVerified) {
-  //   return <>Loading...</>;
-  // }
+  if (!isVerified) {
+    return <>Loading...</>;
+  }
 
   return <>{children}</>;
 };
