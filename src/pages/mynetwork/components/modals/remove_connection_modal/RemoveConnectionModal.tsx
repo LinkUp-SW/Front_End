86628b27c977182c5f47049ce3c3eab09f3fb5fe
NadaOmar03 <nada.mohammad03@eng-st.cu.edu.eg>
2025-04-03 16:34:26 +0200

    fix: enhance modal components with adding dark mode to improve styling

--- src/pages/mynetwork/components/modals/remove_connection_modal/RemoveConnectionModal.tsx
+++ src/pages/mynetwork/components/modals/remove_connection_modal/RemoveConnectionModal.tsx
@@ -16,24 +16,28 @@ const RemoveConnectionModal: React.FC<RemoveConnectionModalProps> = ({
 
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="p-6 w-full md:w-96 lg:w-[30rem] text-center">
-        <h2 className="text-xl font-semibold">Remove Connection</h2>
-        <p className="mt-2">
+      <div className="p-6 w-full md:w-96 lg:w-[30rem] text-center bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+        <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100">
+          Remove Connection
+        </h2>
+        <p className="mt-2 text-gray-700 dark:text-gray-300">
           Are you sure you want to remove <strong>{userData.userName}</strong>?
         </p>
         <div className="mt-4 flex justify-center gap-4">
           <button
+            id="confirm-remove-connection-button"
             onClick={() => {
               onConfirm();
               dispatch(closeModal());
             }}
-            className="px-4 py-2 bg-red-600 text-white rounded cursor-pointer"
+            className="px-4 py-2 bg-red-600 text-white rounded cursor-pointer hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800"
           >
             Confirm
           </button>
           <button
+            id="cancel-remove-connection-button"
             onClick={() => dispatch(closeModal())}
-            className="px-4 py-2 bg-gray-300 rounded cursor-pointer dark:bg-gray-700"
+            className="px-4 py-2 bg-gray-300 text-gray-900 rounded cursor-pointer hover:bg-gray-400 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
           >
             Cancel
           </button>
