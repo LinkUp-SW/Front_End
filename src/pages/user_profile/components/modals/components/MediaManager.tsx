d867632aae850fa7096e783a135c313729c83ddc
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-04-04 10:03:55 +0200

    Ref: Reallocated the MediaManager and SkillsManager components to be in a higher dir. to serve multiple modals

--- src/pages/user_profile/components/modals/components/MediaManager.tsx
+++ src/pages/user_profile/components/modals/components/MediaManager.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { MediaItem } from "../types";
+import { MediaItem } from "./types";
 import { toast } from "sonner";
 
 interface MediaManagerProps {
