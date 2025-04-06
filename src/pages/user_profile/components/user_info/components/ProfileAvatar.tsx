2ec9dbbfbe6f0d19300e8c5bc78ca230f653f210
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-04-06 13:00:02 +0200

    feat: refactor ProfileAvatar to use Dialog for editing profile picture

--- src/pages/user_profile/components/user_info/components/ProfileAvatar.tsx
+++ src/pages/user_profile/components/user_info/components/ProfileAvatar.tsx
@@ -1,5 +1,14 @@
 // components/ProfileAvatar.tsx
-import { FaPencilAlt } from "react-icons/fa";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components";
+import { FcAddImage } from "react-icons/fc";
+import Header from "../../modals/components/Header";
+import ProfilePictureModal from "../../modals/picture_modal/ProfilePictureModal";
 
 type ProfileAvatarProps = {
   src: string;
@@ -15,13 +24,29 @@ export const ProfileAvatar = ({ src, isOwner, onEdit }: ProfileAvatarProps) => (
       className="w-32 h-32 rounded-full border-4 border-white"
     />
     {isOwner && (
-      <button
-        className="absolute hover:opacity-85 transition-all duration-300 cursor-pointer bg-gray-300 dark:bg-gray-800 p-2 rounded-full -bottom-2 right-0"
-        onClick={onEdit}
-        aria-label="Edit profile"
-      >
-        <FaPencilAlt size={20} />
-      </button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <button
+            className="absolute hover:opacity-85 transition-all duration-300 cursor-pointer bg-gray-300 dark:bg-gray-800 p-2 rounded-full -bottom-2 right-0"
+            onClick={onEdit}
+            aria-label="Edit profile"
+            id="edit-profile-button"
+          >
+            <FcAddImage size={20} />
+          </button>
+        </DialogTrigger>
+        <DialogContent
+          aria-describedby={undefined}
+          className="!max-w-5xl md:!w-[43.5rem] dark:bg-gray-900 dark:border-gray-600 !w-full border-2"
+        >
+          <DialogTitle className="hidden"></DialogTitle>
+
+          <DialogHeader id="about-section-dialog-header">
+            <Header title="Profile Photo" />
+          </DialogHeader>
+          <ProfilePictureModal/>
+        </DialogContent>
+      </Dialog>
     )}
   </div>
 );
