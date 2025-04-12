5ec60068925b7f0bf9585eb7153c04a4cad82a71
AmrDoma <amrdoma123@outlook.com>
2025-04-12 19:47:32 +0200

    refactor: refactored my-items page to become one page with state for chosn page saved-jobs or saved-posts

--- src/components/my_items/LeftSideBar.tsx
+++ src/components/my_items/LeftSideBar.tsx
@@ -1,30 +1,74 @@
 import React from "react";
 import { FaBookmark } from "react-icons/fa";
+interface LeftSidebarProps {
+  selectedPage: string;
+  setSelectedPage: (page: string) => void;
+}
 
-const LeftSidebar: React.FC = () => {
+const LeftSidebar: React.FC<LeftSidebarProps> = ({
+  selectedPage,
+  setSelectedPage,
+}) => {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow py-4 pr-4">
-      <h2 className="font-medium text-gray-700 dark:text-gray-200 mb-4 flex items-center">
+    <div className="bg-white dark:bg-gray-800 rounded-lg shadow py-4 pr-0">
+      <h2 className="font-medium text-gray-600  dark:text-gray-200 mb-4 flex items-center pl-4">
         <FaBookmark className="mr-2" />
         <span>My items</span>
       </h2>
 
       <div className="mt-2">
-        <div className="flex justify-between items-center py-2 border-t border-l-4 border-l-blue-500 pl-2">
-          <button id="my-jobs-btn" className="text-blue-600 dark:text-blue-400">
+        <div
+          className={`flex justify-between items-center py-2 border-t pl-4 hover:cursor-pointer hover:underline ${
+            selectedPage === "saved-jobs"
+              ? "border-l-4 border-l-blue-500"
+              : "border-l-transparent"
+          }`}
+          onClick={() => setSelectedPage("saved-jobs")}
+        >
+          <button
+            id="my-jobs-btn"
+            className={`${
+              selectedPage === "saved-jobs"
+                ? "text-blue-600 dark:text-blue-400 font-medium"
+                : "text-gray-600 dark:text-gray-400"
+            }`}
+          >
             My jobs
           </button>
-          <span className="text-blue-600 dark:text-blue-400 text-sm">248</span>
+          <span
+            className={`
+              "text-gray-600 dark:text-gray-400
+              text-sm pr-4`}
+          >
+            248
+          </span>
         </div>
 
-        <div className="flex justify-between items-center py-2">
+        <div
+          className={`flex justify-between items-center py-2 border-t pl-4 hover:cursor-pointer hover:underline  ${
+            selectedPage === "saved-posts"
+              ? "border-l-4 border-l-blue-500 "
+              : "border-l-transparent"
+          }`}
+          onClick={() => setSelectedPage("saved-posts")}
+        >
           <button
             id="saved-posts-btn"
-            className="text-gray-600 dark:text-gray-400"
+            className={`${
+              selectedPage === "saved-posts"
+                ? "text-blue-600 dark:text-blue-400 font-medium"
+                : "text-gray-600 dark:text-gray-400"
+            }`}
           >
             Saved posts and articles
           </button>
-          <span className="text-gray-600 dark:text-gray-400 text-sm">10+</span>
+          <span
+            className={`
+            "text-gray-600 dark:text-gray-400
+            text-sm pr-4`}
+          >
+            10+
+          </span>
         </div>
       </div>
     </div>
