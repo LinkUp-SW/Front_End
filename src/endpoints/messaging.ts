ea8beba7e89d3aa8bf2fb711578828d3d8aa9d63
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-04-18 03:02:44 +0200

    feat:Added the endpoints of getAllConversations,getConversation,deleteConversation,deleteMessages

--- src/endpoints/messaging.ts
+++ src/endpoints/messaging.ts
@@ -1,3 +1,114 @@
+import axiosInstance from "@/services/axiosInstance";
+
+export interface User {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  profilePhoto: string;
+  onlineStatus: boolean;
+}
+
+export interface LastMessage {
+  message: string;
+  timestamp:string | Date; 
+  isOwnMessage: boolean;
+}
+
+export interface Conversation {
+  conversationId: string;
+  conversationType:string[];
+  otherUser: User;
+  lastMessage: LastMessage;
+  unreadCount: number;
+
+}
+
+
+export interface MessageChat
+  {
+    messageId:string
+    senderId: string;
+    senderName: string;
+    message: string;
+    media: string[];
+    timestamp: string | Date ;
+    reacted: boolean;
+    isSeen: boolean;
+    isOwnMessage: boolean;
+  }
+
+export interface chattingMessages{
+  conversationId: string;
+  otherUser: User;
+  messages: MessageChat[];
+}
+
+
+export const getAllConversations = async (
+  token: string
+): Promise<{ conversations: Conversation[] }> => {
+  const response = await axiosInstance.get('/api/v1/conversations', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data; 
+};
+
+export const getConversation = async (
+  token: string,
+  convId: string
+): Promise<chattingMessages> => {
+  const response = await axiosInstance.get(`/api/v1/conversations/${convId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data; 
+};
+
+export const deleteConversation= async(
+  token:string,
+  convId:string
+)=>{
+  const response =await axiosInstance.delete(`/api/v1/conversations/${convId}`,{
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+})
+  return response.data
+}
+
+export const deleteMessages= async(
+  token:string,
+  messageId:string
+)=>{
+  const response =await axiosInstance.delete(`/api/v1/messages/${messageId}`,{
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+})
+  return response.data
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* //add here any needed function that will fetch from an endpoint
 import axios from "axios";
 
