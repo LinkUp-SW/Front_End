8aca157ef60a8cc35304beb92203fa94bba46388
AmrDoma <amrdoma123@outlook.com>
2025-04-19 16:11:14 +0200

    refactor: update WhosHiringImage component to use imported image asset

--- src/components/whos_hiring_image/WhosHiringImage.tsx
+++ src/components/whos_hiring_image/WhosHiringImage.tsx
@@ -1,8 +1,10 @@
+import hiringImage from "../../assets/see_who's_hiring.jpg";
+
 const WhosHiringImage = () => {
   return (
     <div className="mt-4 cursor-pointer h-fit">
       <img
-        src="./src/assets/see_who's_hiring.jpg"
+        src={hiringImage}
         alt="Promotional Banner"
         className="rounded-lg shadow-lg w-58 aspect-square object-fill object-center"
       />
