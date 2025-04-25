2e13a7fd4f1046206615eb688191ffe793745fd7
ghada-elboghdady <115363720+ghada-elboghdady@users.noreply.github.com>
2025-04-25 16:16:20 +0300

    refactor: removed unneeded components

--- src/pages/company/components/manageCompanyPageComponents/SettingsPage.tsx
+++ src/pages/company/components/manageCompanyPageComponents/SettingsPage.tsx
@@ -42,18 +42,6 @@ const SettingsComponent: React.FC<SettingsComponentProps> = ({ companyName = "Yo
             </div>
           </div>
           
-          {/* Manage following */}
-          <div className="border border-gray-200 rounded-lg hover:bg-gray-50 cursor-pointer">
-            <div className="flex items-center justify-between p-4">
-              <div>
-                <h2 className="text-lg font-medium text-gray-800">Manage following</h2>
-                <p className="text-gray-600 mt-1">See all the pages your page follows</p>
-              </div>
-              <div className="text-gray-400">
-                <MdOutlineKeyboardArrowRight className="w-6 h-6" />
-              </div>
-            </div>
-          </div>
           
           {/* Deactivate page */}
           <div 
