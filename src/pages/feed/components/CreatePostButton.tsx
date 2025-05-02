432ff9c8e5f65c1b6c6e0c798186854428824a64
AmrDoma <amrdoma123@outlook.com>
2025-05-02 21:42:39 +0300

    fix: update dark mode hover style for CreatePost button

--- src/pages/feed/components/CreatePostButton.tsx
+++ src/pages/feed/components/CreatePostButton.tsx
@@ -57,7 +57,7 @@ const CreatePost: React.FC<CreatePostProps> = ({ className }) => {
               variant="ghost"
               id="create-post-button"
               onClick={() => dispatch(openCreatePostDialog())}
-              className="w-[90%] h-11 border p-4 hover:bg-gray-200 dark:hover:bg-zinc-800 transition-colors hover:cursor-pointer hover:text-gray-950 dark:hover:text-neutral-200 rounded-full border-gray-400 font-medium text-black focus:outline-none text-left dark:text-neutral-300"
+              className="w-[90%] h-11 border p-4 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors hover:cursor-pointer hover:text-gray-950 dark:hover:text-neutral-200 rounded-full border-gray-400 font-medium text-black focus:outline-none text-left dark:text-neutral-300"
             >
               <p className="w-full">Start a post</p>
             </Button>
