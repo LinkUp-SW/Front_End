cd413415180f3619ea00642bdfeeb1658d4f35e0
NadaOmar03 <nada.mohammad03@eng-st.cu.edu.eg>
2025-05-03 02:15:55 +0300

    fix: update time element className for improved styling in Comment component

--- src/pages/feed/components/Comment.tsx
+++ src/pages/feed/components/Comment.tsx
@@ -416,7 +416,7 @@ const Comment: React.FC<CommentProps> = ({
                     <span>(edited) </span>
                   </>
                 )}
-                <time className="">{timeAgo}</time>
+                <time className="classname pr-2 aw mr-2">{timeAgo}</time>
               </div>
               <Dialog
                 open={deleteModal}
