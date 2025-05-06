d7e4a27ee00f2721461cb8ccc78053727d92c4c7
NadaOmar03 <nada.mohammad03@eng-st.cu.edu.eg>
2025-05-06 23:30:44 +0300

    Remove CatchUp tab component bec no endpoints were provided for it

--- src/pages/mynetwork/MyNetworkPage.tsx
+++ src/pages/mynetwork/MyNetworkPage.tsx
@@ -2,13 +2,10 @@ import { WithNavBar } from "../../components";
 import ManageMyNetwork from "./components/ManageMyNetwork";
 import Invitations from "./components/Invitations";
 import ConnectWithPeople from "./components/ConnectWithPeople";
-import CatchUp from "./components/CatchUp";
-import NetworkNavBar from "./components/NetworkNavBar";
-import { useState } from "react";
 import { ConnectionProvider } from "./components/ConnectionContext";
 
 const MyNetworkPage = () => {
-  const [activeTab, setActiveTab] = useState("grow");
+  
   return (
     <ConnectionProvider>
       <div className="max-w-7xl mx-auto  flex flex-col lg:flex-row gap-6">
@@ -19,15 +16,12 @@ const MyNetworkPage = () => {
 
         {/* Invitations and connect wit people Section */}
         <div className="w-full lg:w-[75%]">
-          <NetworkNavBar setActiveTab={setActiveTab} />
-          {activeTab === "grow" ? (
+         
             <>
               <Invitations />
               <ConnectWithPeople />
             </>
-          ) : (
-            <CatchUp />
-          )}
+          
         </div>
       </div>
     </ConnectionProvider>
